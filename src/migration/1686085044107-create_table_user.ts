fee5903045d5a0c8d06b014e42885c3aa28a9a63
Dikendev <diken.dev@gmail.com>
2023-06-08 23:46:38 -0300

    SQL user table

--- src/migration/1686085044107-create_table_user.ts
+++ src/migration/1686085044107-create_table_user.ts
@@ -3,35 +3,35 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateTableUser1686085044107 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-      CREATE TABLE public.user (
-          id integer NOT NULL,
-          name character varying NOT NULL,
-          email character varying NOT NULL,
-          cpf character varying NOT NULL,
-          type_user int NOT NULL,
-          phone character varying NOT NULL,
-          password character varying NOT NULL,
-          create_at timestamp without time zone DEFAULT now() NOT NULL,
-          updated_at timestamp without time zone DEFAULT now() NO NULL,
-          primary key(id)
-      );
+        CREATE TABLE public.user (
+            id integer NOT NULL,
+            name character varying NOT NULL,
+            email character varying NOT NULL,
+            cpf character varying NOT NULL,
+            type_user int NOT NULL,
+            phone character varying NOT NULL,
+            password character varying NOT NULL,
+            create_at timestamp without time zone DEFAULT now() NOT NULL,
+            updated_at timestamp without time zone DEFAULT now() NO NULL,
+            primary key(id)
+        );
 
-      CREATE SEQUENCE public.user_id_seq
-          AS integer
-          START WITH 1
-          INCREMENT BY 1
-          NO MINVALUE
-          NO MAXVALUE
-          CACHE 1;
-      
-      ALTER SEQUENCE public.user_id_seq OWNER BY public.user.id;
-      ALTER TABLE ONLY public.user ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
-  `);
+        CREATE SEQUENCE public.user_id_seq
+            AS integer
+            START WITH 1
+            INCREMENT BY 1
+            NO MINVALUE
+            NO MAXVALUE
+            CACHE 1;
+        
+        ALTER SEQUENCE public.user_id_seq OWNER BY public.user.id;
+        ALTER TABLE ONLY public.user ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-      drop table public.user;
+        drop table public.user;
     `);
   }
 }
