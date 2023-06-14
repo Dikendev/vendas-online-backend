0477bc6dd767bce3d9e0be3088fffd869b5f5951
Dikendev <diken.dev@gmail.com>
2023-06-13 23:13:20 -0300

    Create user method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/createUser.dto';
 
 @Injectable()
 export class UserService {
@@ -9,6 +10,10 @@ export class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
+
+  async createUser(newUserDTO: CreateUserDto): Promise<UserEntity> {
+    return this.userRepository.save(newUserDTO);
+  }
 
   async getAllUser(): Promise<UserEntity[]> {
     return this.userRepository.find();
