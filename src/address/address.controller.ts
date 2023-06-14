4a5a44099b2ed7d4a1369b360eefac74412169e4
Dikendev <diken.dev@gmail.com>
2023-06-13 23:38:07 -0300

    setting address route

---
+++ src/address/address.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateAddressDTO } from './dto/createAddress.dto';
+import { AddressService } from './address.service';
+import { AddressEntity } from './entities/address.entity';
+
+@Controller('address')
+export class AddressController {
+  constructor(
+    private readonly addressService: AddressService
+  ) {}
+  
+  @Post('/:userId')
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    createAddressDTO: CreateAddressDTO,
+    @Param('userId') userId: number,
+  ): Promise<AddressEntity> {
+    
+    return this.addressService.createAddressDTO(createAddressDTO, userId);
+  }
+}
