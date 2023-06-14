616a5a7561dbb45602d187ebc3f43683252df3bd
Dikendev <diken.dev@gmail.com>
2023-06-13 23:11:45 -0300

    userDto validation

--- src/user/dto/create-user.dto.ts
+++
@@ -1,7 +0,0 @@
-export interface CreateUserDto {
-  nome: string;
-  email: string;
-  phone: string;
-  cpf: string;
-  password: string;
-}
---
+++ src/user/dto/createUser.dto.ts
@@ -0,0 +1,18 @@
+import { IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  nome: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  phone: string;
+
+  @IsString()
+  cpf: string;
+  
+  @IsString()
+  password: string;
+}
