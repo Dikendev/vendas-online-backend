e2a515b037e62d5a0871aca43021f81434e603df
Dikendev <diken.dev@gmail.com>
2023-06-13 23:13:46 -0300

    create user route / get all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserEntity } from './entities/user.entity';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { ReturnUserDto } from './dto/returnUser.dto';
 
 @Controller('user')
 export class UserController {
- constructor(
-  private readonly sev:UserService
- ){}
+  constructor(private readonly userService: UserService) {}
 
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createUser(@Body() newUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(newUser);
+  }
+
+  @UsePipes(ValidationPipe)
   @Get()
-  async getAllUsers(): Promise<UserEntity[]> {
-    return this.sev.getAllUser();
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUser()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
   }
 }
