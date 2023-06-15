a3f1d3f01b99930540d3dd23561db99759be7928
Dikendev <diken.dev@gmail.com>
2023-06-14 22:41:51 -0300

    eslint formatting

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -12,17 +12,15 @@ import { AddressEntity } from './entities/address.entity';
 
 @Controller('address')
 export class AddressController {
-  constructor(
-    private readonly addressService: AddressService
-  ) {}
-  
+  constructor(private readonly addressService: AddressService) {}
+
   @Post('/:userId')
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddressDTO: CreateAddressDTO,
     @Param('userId') userId: number,
   ): Promise<AddressEntity> {
-    
+
     return this.addressService.createAddressDTO(createAddressDTO, userId);
   }
 }
