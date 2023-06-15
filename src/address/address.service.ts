f58c805d103a04a211714e6a6215611738701d1c
Dikendev <diken.dev@gmail.com>
2023-06-14 22:41:06 -0300

    create address

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -3,15 +3,25 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AddressEntity } from './entities/address.entity';
 import { Repository } from 'typeorm';
 import { CreateAddressDTO } from './dto/createAddress.dto';
+import { UserService } from 'src/user/user.service';
+import { CityService } from 'src/city/city.service';
 
 @Injectable()
 export class AddressService {
   constructor(
     @InjectRepository(AddressEntity)
     private readonly addressRepository: Repository<AddressEntity>,
+    private readonly userService: UserService,
+    private readonly cityService: CityService,
   ) {}
 
-  async createAddressDTO(createAddressDTO: CreateAddressDTO, userId: number) {
+  async createAddressDTO(
+    createAddressDTO: CreateAddressDTO,
+    userId: number,
+  ) {
+    await this.userService.getUserById(userId);
+    await this.cityService.findCityById(createAddressDTO.cityId);
+
     return this.addressRepository.save({
       ...createAddressDTO,
       userId,
