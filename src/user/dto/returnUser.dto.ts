7c078f422c68a1ee45240252988d2afedf9f3a77
Dikendev <diken.dev@gmail.com>
2023-06-15 23:21:10 -0300

    address return in dto through map

--- src/user/dto/returnUser.dto.ts
+++ src/user/dto/returnUser.dto.ts
@@ -1,10 +1,11 @@
-import { IsNumber, IsString } from "class-validator";
-import { UserEntity } from "../entities/user.entity";
+import { IsNumber, IsString } from 'class-validator';
+import { UserEntity } from '../entities/user.entity';
+import { ReturnAddressDTO } from 'src/address/dto/returnAddress.dto';
 
 export class ReturnUserDto {
   @IsNumber()
   id: number;
-  
+
   @IsString()
   name: string;
 
@@ -17,11 +18,17 @@ export class ReturnUserDto {
   @IsString()
   cpf: string;
 
+  @IsString()
+  address?: ReturnAddressDTO[];
+
   constructor(userEntity: UserEntity) {
     this.id = userEntity.id;
     this.name = userEntity.name;
     this.email = userEntity.email;
     this.phone = userEntity.phone;
     this.cpf = userEntity.cpf;
+    this.address = userEntity.address
+      ? userEntity.address.map((address) => new ReturnAddressDTO(address))
+      : undefined;
   }
 }
