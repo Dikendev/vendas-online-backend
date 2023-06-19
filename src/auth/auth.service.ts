bd2a1bea49f0da604a2fb04643e9f5931b03e245
Dikendev <diken.dev@gmail.com>
2023-06-18 22:46:38 -0300

    using JWT token to a user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,29 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
+    private jwtService: JwtService,
   ) {}
 
   async signIn(email: string, pass: string): Promise<any> {
     const user = await this.userService.findUserByEmail(email);
 
-    if(user?.password !== pass) {
+    if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    console.log(user.password, pass)
-    const {password, ...result} = user;
 
-    return result;
+    const payload = { sub: user.id, email: user.email };
+    console.log(user.password, pass);
+
+    const { password, ...result } = user;
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      result
+    };
   }
 }
