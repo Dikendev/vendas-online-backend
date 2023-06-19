c6d4978e1b32084125165bd9d57a4bb4508ae290
Dikendev <diken.dev@gmail.com>
2023-06-18 21:36:21 -0300

    authorization controller end point

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,22 @@
 import {
   Body,
   Controller,
+  HttpCode,
+  HttpStatus,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ReturnUserDto } from 'src/user/dto/returnUser.dto';
-import { LoginDTO } from './dtos/login.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @UsePipes(ValidationPipe)
   @Post()
-  async login(@Body() loginDTO: LoginDTO): Promise<ReturnUserDto> {
-    return new ReturnUserDto(await this.authService.login(loginDTO));
+  signIn(@Body() signInDTO: Record<string, any>) {
+    return this.authService.signIn(signInDTO.email, signInDTO.password);
   }
 }
