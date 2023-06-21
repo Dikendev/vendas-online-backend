1050389500868be905581790a61e4abd71d4feb9
Dikendev <diken.dev@gmail.com>
2023-06-21 20:29:59 -0300

    user tests initial settings

--- src/user/__testes__/user.service.spec.ts
+++ src/user/__testes__/user.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../user.service';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { userEntityMock } from '../__mocks__/user.mock';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepository: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(userEntityMock),
+            save: jest.fn().mockResolvedValue(userEntityMock),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  it('should return user in findUserByEmail', async () => {
+    const user = await userService.findUserByEmail(userEntityMock.email);
+
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return error in findUserByEmail', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+
+    expect(
+      userService.findUserByEmail(userEntityMock.email),
+    ).rejects.toThrowError();
   });
 });
